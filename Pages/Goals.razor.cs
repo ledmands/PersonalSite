21627ff454c01769c2024b3a916c80e7ed2a5f5a
ledmands <ledmands+github@proton.me>
2022-11-04 21:22:01 -0400

    created goal cycling button on goal page

---
+++ Pages/Goals.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace PersonalSite.Pages
+{
+    public partial class Goals
+    {
+        public String date;
+        public String goal;
+        public String why;
+        private void DisplayGoal()
+        {
+            int randomGoal = Random.Shared.Next(1, goals.Length);
+            date = goals[randomGoal].Date;
+            goal = goals[randomGoal].Goal;
+            why = goals[randomGoal].Why;
+            
+
+        }
+    }
+}
