85e3287fc15d9d84c84b561b464ede7be0f59a69
ledmands <ledmands+github@proton.me>
2022-11-13 19:03:43 -0500

    Added timer and variables to skills page to enable css changes on clicks, like the memory game, but a lot dumber. Will unlikely need the Http injection and getting info from .txt file, will delete later in cleanup.

---
+++ Pages/Skills.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonalSite.Pages
+{
+    public partial class Skills
+    {
+
+
+        
+        public async Task ShowSkill()
+        {
+
+            currentColor = "skill-card-pink";
+
+            await Task.Delay(3000);
+
+            currentColor = "skill-card-black";
+
+
+        }
+        
+    }
+}
