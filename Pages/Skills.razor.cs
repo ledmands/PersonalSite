40833a862ec167d6072364f49941d3643a94d51a
ledmands <ledmands+github@proton.me>
2022-11-14 18:43:43 -0500

    Skill cards able to be individually controlled, but it's egregeiously in violation of OOP practices...

--- Pages/Skills.razor.cs
+++ Pages/Skills.razor.cs
@@ -2,19 +2,54 @@
 {
     public partial class Skills
     {
+        public String colorTile1 = "skill-card-black";
+        public String colorTile2 = "skill-card-black";
+        public String colorTile3 = "skill-card-black";
+        public String colorTile4 = "skill-card-black";
+        public String colorTile5 = "skill-card-black";
+        public String colorTile6 = "skill-card-black";
 
 
-        
-        public async Task ShowSkill()
+        public async Task ShowSkill(int tileClicked)
         {
+            
+            switch (tileClicked)
+            {
+                case 1:
+                    colorTile1 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile1 = "skill-card-black";
+                    break;
+                case 2:
+                    colorTile2 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile2 = "skill-card-black";
+                    break;
+                case 3:
+                    colorTile3 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile3 = "skill-card-black";
+                    break;
+                case 4:
+                    colorTile4 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile4 = "skill-card-black";
+                    break;
+                case 5:
+                    colorTile5 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile5 = "skill-card-black";
+                    break;
+                case 6:
+                    colorTile6 = "skill-card-pink";
+                    await Task.Delay(3000);
+                    colorTile6 = "skill-card-black";
+                    break;
 
-            currentColor = "skill-card-pink";
-
-            await Task.Delay(3000);
-
-            currentColor = "skill-card-black";
+            }
 
 
+            // switch depending on which tile is passed in
         }
         
     }
