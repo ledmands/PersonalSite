5c237934b517381a128ae9ca8dda1a299210eaca
ledmands <ledmands+github@proton.me>
2022-11-19 19:01:03 -0500

    Think I have to restart... Trying another app...

---
+++ Pages/Email.razor.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Json;
+
+namespace PersonalSite.Pages
+{
+    public partial class Email
+    {
+        // Student model used with form
+        public class EmailData
+        {
+            public int Id { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string Address { get; set; }
+
+            public DateTime? CreatedOn { get; set; }
+        }
+
+        public class EmailApiClient
+        {
+            private readonly HttpClient httpClient;
+
+            public EmailApiClient(HttpClient httpClient)
+            {
+                this.httpClient = httpClient;
+            }
+
+            // Some other methods....
+
+            // Method to create new student record in database
+            public async Task SendEmail(EmailData email)
+            {
+                try
+                {
+                    var response = await httpClient.PostAsJsonAsync<EmailData>("/email", email);
+                }
+                catch (Exception ex)
+                {
+                    // Log error
+                }
+            }
+        }
+    }
+}
